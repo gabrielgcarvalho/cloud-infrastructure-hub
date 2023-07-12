736d4ef58b47527748c643f5bda1a356fc6e246a
Gabriel G <gabrielgcarvalho.br@gmail.com>
2023-07-12 15:08:18 -0300

    Configured backend state for Terraform S3 backend

--- terraform/cloud-infrastructure-hub-account/main.tf
+++ terraform/cloud-infrastructure-hub-account/main.tf
@@ -1,3 +1,14 @@
+terraform {
+
+  backend "s3" {
+    acl            = "bucket-owner-full-control"
+    bucket         = "cloud-infrastructure-hub-terraform-state"
+    dynamodb_table = "cloud-infrastructure-hub-terraform-state-lock"
+    encrypt        = true
+    key            = "cloud-infrastructure-hub-account/terraform.tfstate"
+    region         = "us-east-2"
+  }
+}
 
 # Default provider
 provider "aws" {
