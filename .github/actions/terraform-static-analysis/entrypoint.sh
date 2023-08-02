3f33dcd607e5847b0aa56a7524d7b06e26533699
Gabriel G <gabrielgcarvalho.br@gmail.com>
2023-08-02 01:59:28 -0300

    feat: Comment on pull request TF Lint results

--- .github/actions/terraform-static-analysis/entrypoint.sh
+++ .github/actions/terraform-static-analysis/entrypoint.sh
@@ -7,6 +7,7 @@ echo "All TF directories"
 echo $directories
 
 # TF Lint Configuration
+
 tflint_config="/tflint-configs/tflint.aws.hcl"
 
 echo "Running tflint --init..."
@@ -22,4 +23,31 @@ for directory in $directories; do
     
     # Run tflint on the current directory
     tflint --config "$tflint_config" --chdir "$terraform_working_dir" 2>&1
+
+    tflint_exitcode+=$?
+    echo "tflint_exitcode=${tflint_exitcode}"
 done
+
+if [ $tflint_exitcode -eq 0 ]; then
+  TFLINT_STATUS="Success"
+else
+  TFLINT_STATUS="Failed"
+fi
+
+if [ "${GITHUB_EVENT_NAME}" == "pull_request" ] && [ -n "${GITHUB_TOKEN}" ]; then
+    COMMENT="#### \`Terraform Static Analysis\` 
+
+            #### \`TFLint Scan\` ${TFLINT_STATUS}
+            <details><summary>Show Output</summary>
+
+            \`\`\`hcl
+            ${tflint_exitcode}
+            \`\`\`
+
+            </details>"
+
+    PAYLOAD=$(echo "${COMMENT}" | jq -R --slurp '{body: .}')
+    URL=$(jq -r .pull_request.comments_url "${GITHUB_EVENT_PATH}")
+    echo "${PAYLOAD}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${URL}" > /dev/null
+fi
+
