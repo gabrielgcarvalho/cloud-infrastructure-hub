6a4d01dc703052ff23b024127ba41ed201c0da94
Gabriel G <gabrielgcarvalho.br@gmail.com>
2023-08-02 02:35:10 -0300

    Fix: sh syntax

--- .github/actions/terraform-static-analysis/entrypoint.sh
+++ .github/actions/terraform-static-analysis/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 tflint_exitcode=0
-tflint_output_arr=()
-
-# Initialize the COMMENT variable
-COMMENT=""
+tflint_output=""
 
 # Get a list of all terraform directories in the repo
 directories=$(find . -type f -name '*.tf' | sed 's#/[^/]*$##' | sed 's/.\///' | sort | uniq)
@@ -22,14 +19,15 @@ tflint --init --config "$tflint_config"
 for directory in $directories; do
     echo
     echo "Running tflint in ${directory}"
-    
+
     # Set the working directory for tflint
     terraform_working_dir="${GITHUB_WORKSPACE}/${directory}"
-    
+
     # Run tflint on the current directory and capture the output
-    tflint_output=$(tflint --config "$tflint_config" --chdir "$terraform_working_dir" 2>&1)
-    # Add the tflint output to the array
-    tflint_output_arr+=("${tflint_output}")
+    tflint_output_current=$(tflint --config "$tflint_config" --chdir "$terraform_working_dir" 2>&1)
+    
+    # Add the tflint output to the variable
+    tflint_output="${tflint_output}\n\n#### \`TFLint Output for ${directory}\`\n\`\`\`\n${tflint_output_current}\n\`\`\`"
 
     tflint_exitcode=$((tflint_exitcode + $?))
     echo "tflint_exitcode=${tflint_exitcode}"
@@ -41,16 +39,10 @@ else
   TFLINT_STATUS="\033[31mFailed\033[0m"
 fi
 
-
-# Concatenate the tflint output from the array into the COMMENT variable
-for tflint_output in "${tflint_output_arr[@]}"; do
-  COMMENT+="\n\n#### \`TFLint Output for ${directory}\`\n\`\`\`\n${tflint_output}\n\`\`\`"
-done
-
 if [ "${GITHUB_EVENT_NAME}" = "pull_request" ] && [ -n "${GITHUB_TOKEN}" ]; then
     COMMENT="## \`Terraform Static Analysis\`
 #### \`TFLint Scan\` ${TFLINT_STATUS}
-<details><summary>Show Output</summary>${COMMENT}</details>"
+<details><summary>Show Output</summary>${tflint_output}</details>"
 
     PAYLOAD=$(echo "${COMMENT}" | jq -R --slurp '{body: .}' -c)
     URL=$(jq -r .pull_request.comments_url "${GITHUB_EVENT_PATH}")
