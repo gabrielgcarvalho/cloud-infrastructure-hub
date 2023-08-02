3e7652cdb588112586dd236a74a615ceb3ee0042
Gabriel G <gabrielgcarvalho.br@gmail.com>
2023-08-02 01:33:43 -0300

    refact: Utilize TFLint for Terraform Static Analysis instead of Terraform Format

--- .github/actions/terraform-static-analysis/entrypoint.sh
+++ .github/actions/terraform-static-analysis/entrypoint.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
-terraform_files=$(find . -type f -name "*.tf")
+# Get a list of all terraform directories in the repo
+directories=$(find . -type f -name '*.tf' | sed 's#/[^/]*$##' | sed 's/.\///' | sort | uniq)
+echo
+echo "All TF directories"
+echo $directories
 
-if [ -z "$terraform_files" ]; then
-    echo "No Terraform files found."
-    exit 0
-fi
+# TF Lint Configuration
+tflint_config="/tflint-configs/tflint.aws.hcl"
 
-for file in $terraform_files; do
-    echo "Checking $file"
-    terraform fmt -check "$file"
+echo "Running tflint --init..."
+tflint --init --config "$tflint_config"
+
+# Loop through each Terraform directory
+for directory in $directories; do
+    echo
+    echo "Running tflint in ${directory}"
+    
+    # Set the working directory for tflint
+    terraform_working_dir="${GITHUB_WORKSPACE}/${directory}"
+    
+    # Run tflint on the current directory
+    tflint --config "$tflint_config" --chdir "$terraform_working_dir" 2>&1
 done
