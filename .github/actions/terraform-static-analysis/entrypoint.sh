96f2046a64f5a55760e5295275c9bf81ffe1089f
Gabriel G <gabrielgcarvalho.br@gmail.com>
2023-08-02 02:42:50 -0300

    refact: Improve Terraform Static Analysis PR COMMENT

--- .github/actions/terraform-static-analysis/entrypoint.sh
+++ .github/actions/terraform-static-analysis/entrypoint.sh
@@ -26,23 +26,23 @@ for directory in $directories; do
     # Run tflint on the current directory and capture the output
     tflint_output_current=$(tflint --config "$tflint_config" --chdir "$terraform_working_dir" 2>&1)
     
-    # Add the tflint output to the variable
-    tflint_output="${tflint_output}\n\n#### \`TFLint Output for ${directory}\`\n\`\`\`\n${tflint_output_current}\n\`\`\`"
+     # Add the tflint output to the variable
+    tflint_output+="\n\n<details><summary>TFLint Output for ${directory}</summary>\n\n\`\`\`hcl\n${tflint_output_current}\n\`\`\`\n</details>"
 
     tflint_exitcode=$((tflint_exitcode + $?))
     echo "tflint_exitcode=${tflint_exitcode}"
 done
 
 if [ $tflint_exitcode -eq 0 ]; then
-  TFLINT_STATUS="\033[32mSuccess\033[0m"
+  TFLINT_STATUS=":white_check_mark: Success"
 else
-  TFLINT_STATUS="\033[31mFailed\033[0m"
+  TFLINT_STATUS=":x: Failed"
 fi
 
 if [ "${GITHUB_EVENT_NAME}" = "pull_request" ] && [ -n "${GITHUB_TOKEN}" ]; then
-    COMMENT="## \`Terraform Static Analysis\`
-#### \`TFLint Scan\` ${TFLINT_STATUS}
-<details><summary>Show Output</summary>${tflint_output}</details>"
+    COMMENT="## Terraform Static Analysis
+#### TFLint Scan Status: ${TFLINT_STATUS}
+${tflint_output}"
 
     PAYLOAD=$(echo "${COMMENT}" | jq -R --slurp '{body: .}' -c)
     URL=$(jq -r .pull_request.comments_url "${GITHUB_EVENT_PATH}")
