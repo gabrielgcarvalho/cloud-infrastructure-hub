283b50d468a0fabcc4de51053771980294a650a7
Gabriel G <gabrielgcarvalho.br@gmail.com>
2023-08-02 03:57:00 -0300

    fix: sh syntax and capture the exit code of the tflint command

--- .github/actions/terraform-static-analysis/entrypoint.sh
+++ .github/actions/terraform-static-analysis/entrypoint.sh
@@ -25,15 +25,16 @@ for directory in $directories; do
 
     # Run tflint on the current directory and capture the output
     tflint_output_current=$(tflint --config "$tflint_config" --chdir "$terraform_working_dir" 2>&1)
-    
+
+    # Capture the exit code of the tflint command
+    tflint_exitcode=$((tflint_exitcode + $?))
+    echo "tflint_exitcode=${tflint_exitcode}"
+
     # Add the tflint output for the current directory to the variable if the current output is not empty
-    if [ -n ${tflint_output_current} ]; then
+    if [ -n "${tflint_output_current}" ]; then
         tflint_output="${tflint_output}<br><details><summary>:mag: <strong>TFLint Output for ${directory}</strong></summary><br>${tflint_output_current}<br></details>"
     fi
 
-
-    tflint_exitcode=$((tflint_exitcode + $?))
-    echo "tflint_exitcode=${tflint_exitcode}"
 done
 
 if [ $tflint_exitcode -eq 0 ]; then
