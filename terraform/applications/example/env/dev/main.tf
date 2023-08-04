440311b5df863e6346c28b0f4f9adfbaea223489
Gabriel G <gabrielgcarvalho.br@gmail.com>
2023-08-04 02:09:26 -0300

    chore: update instance type

--- terraform/applications/example/env/dev/main.tf
+++ terraform/applications/example/env/dev/main.tf
@@ -1,7 +1,7 @@
 
 resource "aws_instance" "example" {
   ami                    = var.ami
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = var.user_data_base64
