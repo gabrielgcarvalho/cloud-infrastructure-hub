9846fd09dd3c1c6dacee02469c89b38bd9fde77c
Gabriel G <gabrielgcarvalho.br@gmail.com>
2023-08-05 03:19:15 -0300

    Fix: terraform-plan.sh and add GITHUB_TOKEN env for Terraform Plan step

--- scripts/terraform-plan.sh
+++ scripts/terraform-plan.sh
@@ -18,4 +18,4 @@ terraform -chdir="${terraform_working_dir}" plan -input=false -no-color | \
       -e 's/AWS_ACCESS_KEY_ID".*/<REDACTED>/g' \
       -e 's/$AWS_SECRET_ACCESS_KEY".*/<REDACTED>/g' \
       -e 's/$AWS_ACCESS_KEY_ID".*/<REDACTED>/g' \
-      -e 's/\[id=.*\]/\[id=<REDACTED>\]/g' | tee /dev/stderr | grep '^Plan: \|^No changes.')
+      -e 's/\[id=.*\]/\[id=<REDACTED>\]/g' | tee /dev/stderr | grep '^Plan: \|^No changes.'
