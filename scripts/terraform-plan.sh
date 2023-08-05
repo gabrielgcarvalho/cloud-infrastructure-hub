8eb5563c2eabe25924947868eacd3e63a6e9bf5b
Gabriel G <gabrielgcarvalho.br@gmail.com>
2023-08-05 03:08:58 -0300

    Feat: Add terraform plan and comment-pr scripts | update example_ci

---
+++ scripts/terraform-plan.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o pipefail
+set -e
+
+# Script to run Terraform plan in a specific directory
+# Usage: ./terraform-plan.sh <directory_path>
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <directory_path>"
+  exit 1
+fi
+
+terraform_working_dir="$1"
+
+terraform -chdir="${terraform_working_dir}" plan -input=false -no-color | \
+  sed -e 's/AWS_SECRET_ACCESS_KEY".*/<REDACTED>/g' \
+      -e 's/AWS_ACCESS_KEY_ID".*/<REDACTED>/g' \
+      -e 's/$AWS_SECRET_ACCESS_KEY".*/<REDACTED>/g' \
+      -e 's/$AWS_ACCESS_KEY_ID".*/<REDACTED>/g' \
+      -e 's/\[id=.*\]/\[id=<REDACTED>\]/g' | tee /dev/stderr | grep '^Plan: \|^No changes.')
